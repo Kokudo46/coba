bc9d33f527db0be499011d62a928faac0ac5dbf6
kokudo <kokudo26@gmail.com>
2026-05-21 15:35:35 -0700

    Implement CORS support in index.php
    Add CORS headers to handle cross-origin requests.

--- public/index.php
+++ public/index.php
@@ -1,5 +1,15 @@
 <?php
 
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Access-Key");
+header("Access-Control-Max-Age: 86400");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
+
 /**
  * Import semua class yang digunakan dalam framework ini.
  * Tenang, ini telah dilakukan secara otomatis oleh composer.
