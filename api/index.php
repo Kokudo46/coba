14f910f07ba4311a2211a79386d28787c1919a49
kokudo <kokudo26@gmail.com>
2026-05-21 16:07:02 -0700

    Implement CORS headers in index.php
    Add CORS headers to allow cross-origin requests.

--- api/index.php
+++ api/index.php
@@ -1,4 +1,13 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Credentials: true");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Access-Key");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
 
 /**
  * Redirect request to public
